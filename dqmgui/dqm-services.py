9b674f5173dd4ddb99519df762a5bc5992ce8f5a
ECAL PRO <ecalpro@cms>
2016-04-15 17:10:48 +0200

    Fix server name

--- dqmgui/dqm-services.py
+++ dqmgui/dqm-services.py
@@ -1,3 +1,3 @@
-server.service('ECAL DQM inside P5','http://fu-c2f11-21-02.cms:8030/dqm/ecal/')
+server.service('ECAL DQM inside P5','http://ecalod-dqm.cms:8030/dqm/ecal/')
 server.service('CMS DQM inside P5','http://dqm-prod-local.cms:8030/dqm/online/')
 server.service('CMS DQM outside P5','http://cmsweb.cern.ch/dqm/online/')
